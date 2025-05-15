b50b83bef33351b152df2b59ff1fd736f6b40fa4
Yoilin Mejía <119643387+Itsyoiyo@users.noreply.github.com>
2025-05-14 19:38:05 -0600

    PRIMER COMMIT

---
+++ src/test/java/com/TiendaII/TiendaiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TiendaII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
