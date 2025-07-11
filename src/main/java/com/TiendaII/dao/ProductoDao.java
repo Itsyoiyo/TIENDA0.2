e41734a09dc2e2c4b3b7bc39efa9671897c8e3d7
Yoilin Mejía <119643387+Itsyoiyo@users.noreply.github.com>
2025-07-11 11:39:22 -0600

    COMMIT 8(PRACTICA 4 INDIDIVUAL)

--- src/main/java/com/TiendaII/dao/ProductoDao.java
+++ src/main/java/com/TiendaII/dao/ProductoDao.java
@@ -16,8 +16,19 @@ public interface ProductoDao extends JpaRepository <Producto, Long>{
     @Query(value="SELECT a FROM Producto a where a.precio BETWEEN :precioInf AND :precioSup ORDER BY a.descripcion ASC")
     public List<Producto> metodoJPQL(@Param("precioInf") double precioInf, @Param("precioSup") double precioSup);
    //Ejemplo de método utilizando Consultas con SQL nativo
-    @Query(nativeQuery=true,
-            value="SELECT * FROM producto where producto.precio BETWEEN :precioInf AND :precioSup ORDER BY producto.descripcion ASC")
-    public List<Producto> metodoNativo(@Param("precioInf") double precioInf, @Param("precioSup") double precioSup); 
-    
+    @Query(nativeQuery = true,
+            value = "SELECT * FROM producto where producto.precio BETWEEN :precioInf AND :precioSup ORDER BY producto.descripcion ASC")
+    public List<Producto> metodoNativo(@Param("precioInf") double precioInf, @Param("precioSup") double precioSup);
+
+// Query Method
+public List<Producto> findByExistenciasBetweenOrderByDescripcion(int existenciasMin, int existenciasMax);
+
+// JPQL
+@Query("SELECT p FROM Producto p WHERE p.existencias BETWEEN :existenciasMin AND :existenciasMax ORDER BY p.descripcion ASC")
+public List<Producto> buscarPorExistenciasJPQL(@Param("existenciasMin") int existenciasMin, @Param("existenciasMax") int existenciasMax);
+
+// SQL Nativo
+@Query(nativeQuery = true, value = "SELECT * FROM producto WHERE existencias BETWEEN :existenciasMin AND :existenciasMax ORDER BY descripcion ASC")
+public List<Producto> buscarPorExistenciasNativo(@Param("existenciasMin") int existenciasMin, @Param("existenciasMax") int existenciasMax);
+
 }
--- src/main/java/com/TiendaII/service/ProductoService.java
+++ src/main/java/com/TiendaII/service/ProductoService.java
@@ -25,7 +25,14 @@ public interface ProductoService {
 
     //Lista de productos utilizando consultas con JPQL    
     public List<Producto> metodoJPQL(double precioInf, double precioSup);
-     //Lista de productos utilizando consultas con SQL Nativo
+    //Lista de productos utilizando consultas con SQL Nativo
+
     public List<Producto> metodoNativo(double precioInf, double precioSup);
+
+public List<Producto> findByExistenciasBetweenOrderByDescripcion(int existenciasMin, int existenciasMax);
+
+public List<Producto> buscarPorExistenciasJPQL(int existenciasMin, int existenciasMax);
+
+public List<Producto> buscarPorExistenciasNativo(int existenciasMin, int existenciasMax);
 
 }
