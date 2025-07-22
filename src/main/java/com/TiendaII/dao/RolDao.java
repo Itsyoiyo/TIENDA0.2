8e16e029305d79fd324807392416ff5e3a574e19
Yoilin Mejía <119643387+Itsyoiyo@users.noreply.github.com>
2025-07-21 21:01:41 -0600

    semana 11

---
+++ src/main/java/com/TiendaII/dao/RolDao.java
@@ -0,0 +1,8 @@
+package com.TiendaII.dao;
+
+import com.TiendaII.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolDao extends JpaRepository<Rol, Long> {
+
+}
--- src/main/java/com/TiendaII/dao/UsuarioDao.java
+++ src/main/java/com/TiendaII/dao/UsuarioDao.java
@@ -3,6 +3,14 @@ package com.TiendaII.dao;
 import com.TiendaII.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioDao extends JpaRepository<Usuario, Long> {    
+public interface UsuarioDao extends JpaRepository<Usuario, Long> {
+
     Usuario findByUsername(String username); //asociaciones a traves de un prefijo
+
+    Usuario findByUsernameAndPassword(String username, String Password);
+
+    Usuario findByUsernameOrCorreo(String username, String correo);
+
+    boolean existsByUsernameOrCorreo(String username, String correo);
+
 }
