a4fab9dee706ec9caa1dbcd37ba7fc533a23990d
Yoilin Mejía <119643387+Itsyoiyo@users.noreply.github.com>
2025-07-28 19:29:34 -0600

    semana 12

---
+++ src/main/java/com/TiendaII/dao/VentaDao.java
@@ -0,0 +1,9 @@
+
+package com.TiendaII.dao;
+
+import com.TiendaII.domain.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VentaDao extends JpaRepository <Venta,Long> {
+     
+}
